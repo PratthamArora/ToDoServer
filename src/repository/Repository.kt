24b898ce8038917de625cf1104af4ca334b4b51d
PratthamArora <arora.prattham@gmail.com>
2020-07-22 19:21:40 +0530

    added repository

---
+++ src/repository/Repository.kt
@@ -0,0 +1,16 @@
+package com.pratthamarora.repository
+
+import com.pratthamarora.model.User
+
+interface Repository {
+
+    suspend fun addUser(
+        email: String,
+        displayName: String,
+        passwordHash: String
+    ): User?
+
+    suspend fun findUser(userId: Int): User?
+
+    suspend fun findUserByEmail(email: String): User?
+}
